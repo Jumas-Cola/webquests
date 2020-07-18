4e86e96e6709f3c251d5bd5b5c49babfd963f609
Jumas-Cola <kbbyfl91@gmail.com>
2020-07-19 00:44:28 +0300

    fix congrats.js img selector

--- ciphers_webquest/js/congrats.js
+++ ciphers_webquest/js/congrats.js
@@ -17,18 +17,18 @@ function exportCanvasAsPNG(id, fileName) {
 }
 
 var canvas = document.getElementById('canvas');
-var width = $('img').width();
-var height = $('img').height();
+var width = $('#img-certificate').width();
+var height = $('#img-certificate').height();
 ctx = canvas.getContext('2d');
 canvas.width = width;
 canvas.crossOrigin = "anonymous";
 canvas.height = height;
-ctx.drawImage($('img').get(0), 0, 0);
+ctx.drawImage($('#img-certificate').get(0), 0, 0);
 ctx.font = "50pt Verdana";
 $('#get-certificate').click(function(){
     //redraw image
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.drawImage($('img').get(0), 0, 0);
+    ctx.drawImage($('#img-certificate').get(0), 0, 0);
     //refill text
     ctx.fillStyle = "#e55e5e";
     ctx.fillText($('#surname').val(), Math.floor(width/3), Math.floor(height/2) + 20);
